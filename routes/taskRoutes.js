165fe028e51ffb23bde9fdcef635051e77036e4c
Mohaalc <makhadar126@gmail.com>
2024-10-31 14:15:50 -0700

    routes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,20 +1,25 @@
-const { getTasks, createTask, updateTask, deleteTask } = require("../controllers/taskControllers");
-
-const taskRoutes = (req, res) => {
-    if (req.method === 'GET') {
-        getTasks(req, res);
-    } else if (req.method === 'POST') {
-        createTask(req, res);
-    } else if (req.method === 'PATCH') {
-        updateTask(req, res);
-    } else if (req.method === 'DELETE') {
-        deleteTask(req, res);
-    } else {
-        res.writeHead(404, 'Not found', {'content-type': 'application/json'});
-        res.end(JSON.stringify({
-            message: "Unknown method required."
-        }));
+const taskroutes=(req,res) =>{
+    if(req.method=='GET'){
+        getTasks(req,res); 
     }
-};
+    else if(req.method=='POST'){
+        createTask(req,res)  
+    }
+    else if(req.method == 'PATCH'){
+      
+        updateTask(req,res)
+    }
+    else if(req.method == 'DELETE'){
+        deleteTask(req,res)
+    }
+    else{
+        res.writehead(404,'not found',{'content_type':'application/json'
+        })
+        res.end(JSON.stringify({
+            message:'unkown method required'
+        }))
+         
+        }
+    }
 
-module.exports = taskRoutes;
+module.exports=taskroutes;
